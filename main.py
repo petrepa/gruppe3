36f29e5a05f68863228e58a96ee29cb90d7bce3c
Peter Remøy Paulsen <peter.remoy.paulsen@gmail.com>
2018-04-05 11:12:52 +0200

    Lagt til ein historisk-modul

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ from kivy.core.image import Image
 from kivy.graphics import Color, Rectangle
 from kivy.clock import Clock
 from kivy.core.audio import SoundLoader,Sound
-
-
-
-
+from kivy.uix.scrollview import ScrollView
 
 
 
@@ -27,12 +24,40 @@ class MainWidget(Screen):
 
 #Funksjonane til dei ulike modulane
 class KlimaScreen(Screen):
-	M = SoundLoader.load('klimalyd/test.mp3')
-	def plays(self):
-	    if KlimaScreen.M.state == 'stop':
-	        KlimaScreen.M.play()
+	S = SoundLoader.load('klimalyd/basseng.mp3')
+	def playsS(self):
+	    if KlimaScreen.S.state == 'stop':
+	        KlimaScreen.S.play()
 	    else:
-	        KlimaScreen.M.stop()
+	        KlimaScreen.S.stop()
+	H = SoundLoader.load('klimalyd/hav.mp3')
+	def playsH(self):
+	    if KlimaScreen.H.state == 'stop':
+	        KlimaScreen.H.play()
+	    else:
+	        KlimaScreen.H.stop()
+	J = SoundLoader.load('klimalyd/jungel.mp3')
+	def playsJ(self):
+	    if KlimaScreen.J.state == 'stop':
+	        KlimaScreen.J.play()
+	    else:
+	        KlimaScreen.J.stop()
+	I = SoundLoader.load('klimalyd/hav.mp3')
+	def playsI(self):
+	    if KlimaScreen.I.state == 'stop':
+	        KlimaScreen.I.play()
+	    else:
+	        KlimaScreen.I.stop()
+
+	def stopAll(self):
+		if KlimaScreen.S.state == 'play':
+			KlimaScreen.S.stop()
+		if KlimaScreen.H.state == 'play':
+			KlimaScreen.H.stop()
+		if KlimaScreen.J.state == 'play':
+			KlimaScreen.J.stop()
+		if KlimaScreen.I.state == 'play':
+			KlimaScreen.I.stop()
 	
 
 class HistorieScreen(Screen):
