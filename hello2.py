cb2f87d09361fe00ecd4733700445ac975e87154
Peter Remøy Paulsen <peter.remoy.paulsen@gmail.com>
2018-04-25 20:48:40 +0200

    Laga til hello world test2

---
+++ hello2.py
@@ -0,0 +1,15 @@
+import kivy
+
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class HeiApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    HeiApp().run()
